9a1fdfc4f3a6ee30052c63a59b2fd590d280c68c
Paul Duvall <duvallpj@gmail.com>
2026-02-27 08:04:38 +0000

    feat: add page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,16 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	css: ['@picocss/pico', '~/assets/variables.css', '~/assets/base.css'],
 	compatibilityDate: "2025-07-15",
+	app: {
+		head: {
+			title: "Ways Unseen",
+			meta: [
+				{ charset: "utf-8" },
+				{ name: "viewport", content: "width=device-width, initial-scale=1" },
+				{ name: "description", content: "Ways Unseen - Art Portfolio" },
+			],
+		},
+	},
 	eslint: {
 		config: {
 			stylistic: {
