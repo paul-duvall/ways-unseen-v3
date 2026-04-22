f9b4bce6c0f7eb3a8e0c1da0d78a3b6091515e74
Paul Duvall <duvallpj@gmail.com>
2026-04-22 20:43:02 +0100

    feat: add free map terms page

---
+++ app/pages/free-maps-terms.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="container">
+        <section>
+            <h2>Free Map Pack - Terms of use</h2>
+            <h3>The short version:</h3>
+            <p>
+                You can use the maps in the Free Map Pack by <em>Ways Unseen</em> for personal use or in commercial projects,
+                including role-playing and tabletop games or resources.
+            </p>
+            <p>
+                These terms assume there are no sexist, racist, homophobic and transphobic tropes and themes in your content.
+                If subsequently this is found not to be the case, all rights are terminated.
+            </p>
+
+            <h3>The long version:</h3>
+            <p>These images may be used under the terms of the following non-exclusive license:</p>
+            <p>
+                You may use the images in products either for print or web distribution, along with associated advertising.
+                This use may be for commercial purposes as part of a publication that constitutes a larger body of work not
+                limited to the images alone.
+            </p>
+            <p>
+                You may reduce the size of the images; you may crop the images. However, if you crop out the artist's
+                identifying mark then you must include the following statement near the image:
+            </p>
+            <p><strong>&copy; <em>Ways Unseen</em></strong></p>
+            <p>You may not colour, modify or adapt the images.</p>
+            <p>
+                You may not use the images as the basis for any individual or collection of physical or digital merchandise
+                or promotional items, which you offer for sale, such as but not limited to t-shirts, digital or physical
+                greeting cards, mugs, posters, prints, and so on.
+            </p>
+            <p>
+                You cannot resell these images or redistribute them in any way that competes with the original map pack.
+            </p>
+            <p>
+                You cannot use these images (in whole or in part) as a trademark, service mark, logo or other indication of
+                origin, or as part thereof, or to otherwise endorse or imply the endorsement of any goods or services.
+            </p>
+            <p>
+                You are not authorised to sub-license, sell or lease the images, nor may you temporarily or permanently
+                transfer your right under this Agreement to a third party. This includes, by way of illustrations only and
+                not by way of limitation, displaying images in any format (including thumbnails) for download on a website,
+                offering the images for sale, or including images in templates of any nature, including web templates,
+                document templates, projects, or otherwise for distribution and/or sale to third parties.
+            </p>
+            <p>
+                If you use any of these images in a publication (digital, print, or otherwise) you must include the
+                following statements in the credits:
+            </p>
+            <p><strong>Map &copy; <em>Ways Unseen</em>, used with permission. All rights reserved.</strong></p>
+            <p>
+                Except for your right to use these maps in this collection granted by this licence, all other rights,
+                title, and interest in the images and related trademarks and trade names are owned and retained by
+                <em>Ways Unseen</em>.
+            </p>
+            <p>
+                This agreement will immediately and automatically terminate without notice if you fail to comply with any
+                term or condition of this Agreement. If this Agreement is terminated, you agree to destroy all copies of
+                the images and documentation in your possessions, including any product or publication within which you have
+                employed the images.
+            </p>
+            <p><em>Ways Unseen</em> takes no responsibility for any legal claims against your product.</p>
+            <p>
+                If you have any questions, please contact:
+                <a href="mailto:ways.unseen@gmail.com">ways.unseen@gmail.com</a>
+            </p>
+        </section>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    background-color: var(--primary-color-dark);
+    padding: 2rem;
+}
+@media (max-width: 768px) {
+    .container {
+        padding: 1rem;
+    }
+}
+h2 {
+    text-align: center;
+}
+section {
+    margin: 0 auto 2rem;
+    max-width: 45rem;
+}
+</style>
